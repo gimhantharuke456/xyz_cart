f6d24889cce8a64f40e94e72ed6945a2cd6db017
gimhantharuke456 <gimhantharuke456@gmail.com>
2023-09-09 12:07:48 +0530

    ADD:internet permissions added

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -4,4 +4,11 @@ import 'package:xyz_cart/utils/constants.dart';
 ThemeData get appTheme => ThemeData(
       scaffoldBackgroundColor: secondaryColor,
       primaryColor: primaryColor,
+      iconTheme: const IconThemeData(
+        color: Colors.white,
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.white,
+      ),
     );
